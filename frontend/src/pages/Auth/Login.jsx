7bad71a8d3d22321b988ca5bbef5fb3ebcdb0b45
Sam <soham.mhatre97@gmail.com>
2024-03-03 15:40:27 -0500

    Login Functionality Implemented

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useLoginMutation } from "../../redux/api/usersApiSlice";
 import { setCredentials } from "../../redux/features/auth/authSlice";
 import { toast } from "react-toastify";
+import Loader from "../../componenets/Loader";
 
 
 const Login = () => {
@@ -27,10 +28,72 @@ const Login = () => {
     }
   }, [navigate, redirect, userInfo])
 
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    try{
+        const res = await login({ email, password}).unwrap()
+        console.log(res);
+        dispatch(setCredentials({...res}));
+    } catch (error) {
+        toast.error(error?.data?.message || error.message)
+    }
+  }
+
   return <div>
     <section className="pl-[10rem] flex flex-wrap">
         <div className="mr-[4rem] mt-[5rem]">
             <h1 className="text-2xl font-semibold mb-4">Sign In</h1>
+            <form onSubmit={submitHandler} className="container w-[40rem]">
+                <div className="my-[2rem]">
+                    <label 
+                        htmlFor="email"
+                        className="block text-sm font-medium text-white"
+                    >
+                        Email Address
+                    </label>
+                    <input type="email" 
+                        id="email" 
+                        className="mt-1 p-2 border rounder w-full" 
+                        value={email} 
+                        onChange={e => setEmail(e.target.value)}>
+                    </input>
+                </div>
+
+                <div className="my-[2rem]">
+                    <label 
+                        htmlFor="password"
+                        className="block text-sm font-medium text-white"
+                    >
+                        Password
+                    </label>
+                    <input type="password" 
+                        id="password" 
+                        className="mt-1 p-2 border rounder w-full" 
+                        value={password} 
+                        onChange={e => setPassword(e.target.value)}>
+                    </input>
+                </div>
+
+                <button
+                disabled={isLoading}
+                type="submit"
+                className="bg-pink-500 text-white px-4 py-2 rounded curson-pointer my-[1rem]">
+                    {isLoading ? "Signing In..." : "Sign In"}
+                </button>
+                {isLoading && <Loader />}
+            </form>
+            <div className="mt-4">
+                <p className="text-white">
+                    New Customer ? {" "}
+                    <Link 
+                        to={redirect ? `/register?redirect=${redirect}` : "/regsiter"}
+                        className="text-pink-500 hover:underline"
+                    >
+                        Register
+                    </Link>
+                </p>
+            </div>
         </div>
     </section>
   </div>;
